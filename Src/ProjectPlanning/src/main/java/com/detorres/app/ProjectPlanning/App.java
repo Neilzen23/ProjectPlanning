dca11e3789fab0b406e6730dc8c68b4bb6a61237
AzureAD\NeilzenAnthonyDETORR <detorresneilzen109@gmail.com>
2019-01-16 03:19:42 -0500

    Upload Base to Master

---
+++ Src/ProjectPlanning/src/main/java/com/detorres/app/ProjectPlanning/App.java
@@ -0,0 +1,13 @@
+package com.detorres.app.ProjectPlanning;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
